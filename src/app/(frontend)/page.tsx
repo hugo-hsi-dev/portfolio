ff8860d034e5134d72604bf25b3a23e93ee9d605
Hugo Hsi <hugohsidev@gmail.com>
2024-09-10 21:47:26 -0400

    adds image to root page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,18 @@
-export default function RootPage() {
-  return <div>page</div>
+import { Media } from '@/payload-types'
+import config from '@payload-config'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import Image from 'next/image'
+export default async function RootPage() {
+  const payload = await getPayloadHMR({ config })
+  const { openGraphImage } = await payload.findGlobal({
+    slug: 'meta',
+  })
+
+  const image = openGraphImage as Media
+
+  return (
+    <div>
+      <Image src={image.url!} alt={image.alt} width={240} height={240} />
+    </div>
+  )
 }
