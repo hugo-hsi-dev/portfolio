9e98350247ff1ab81801143374d4ea05a3c08780
Hugo Hsi <hugohsidev@gmail.com>
2024-09-16 18:46:21 -0400

    adds links to contact expanded block

--- src/components/block/blockTypes/contact/contactListItem/items/GithubListItem.tsx
+++ src/components/block/blockTypes/contact/contactListItem/items/GithubListItem.tsx
@@ -3,18 +3,23 @@ import ContactListIcon from '@/components/block/blockTypes/contact/contactListIt
 import ContactListItem from '@/components/block/blockTypes/contact/contactListItem/ContactListItem'
 import ContactListURL from '@/components/block/blockTypes/contact/contactListItem/ContactListURL'
 import { Github } from 'lucide-react'
+import Link from 'next/link'
 
 type GithubListItemProps = { githubUrl: string }
 
 export default function GithubListItem({ githubUrl }: GithubListItemProps) {
   return (
     <ContactListItem>
-      <ContactListIcon>
-        <Github className="text-white" width={50} height={50} />
-      </ContactListIcon>
-      <ContactListURL>
-        <TextUnderline>{githubUrl}</TextUnderline>
-      </ContactListURL>
+      <Link href={githubUrl} target="_blank">
+        <ContactListIcon>
+          <Github className="text-white" width={50} height={50} />
+        </ContactListIcon>
+      </Link>
+      <Link href={githubUrl} target="_blank">
+        <ContactListURL>
+          <TextUnderline>{githubUrl}</TextUnderline>
+        </ContactListURL>
+      </Link>
     </ContactListItem>
   )
 }
--- src/components/block/blockTypes/contact/contactListItem/items/LinkedInListItem.tsx
+++ src/components/block/blockTypes/contact/contactListItem/items/LinkedInListItem.tsx
@@ -3,18 +3,23 @@ import ContactListIcon from '@/components/block/blockTypes/contact/contactListIt
 import ContactListItem from '@/components/block/blockTypes/contact/contactListItem/ContactListItem'
 import ContactListURL from '@/components/block/blockTypes/contact/contactListItem/ContactListURL'
 import { Linkedin } from 'lucide-react'
+import Link from 'next/link'
 
 type LinkedinListItemProps = { linkedinUrl: string }
 
 export default function LinkedInListItem({ linkedinUrl }: LinkedinListItemProps) {
   return (
     <ContactListItem>
-      <ContactListIcon>
-        <Linkedin className="text-white" width={50} height={50} />
-      </ContactListIcon>
-      <ContactListURL>
-        <TextUnderline>{linkedinUrl}</TextUnderline>
-      </ContactListURL>
+      <Link href={linkedinUrl} target="_blank">
+        <ContactListIcon>
+          <Linkedin className="text-white" width={50} height={50} />
+        </ContactListIcon>
+      </Link>
+      <Link href={linkedinUrl} target="_blank">
+        <ContactListURL>
+          <TextUnderline>{linkedinUrl}</TextUnderline>
+        </ContactListURL>
+      </Link>
     </ContactListItem>
   )
 }
